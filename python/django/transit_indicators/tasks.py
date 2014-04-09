1e0cb48330a9df6e60b1ae312846b87a255ec395
Kathryn Killebrew <kkillebrew@azavea.com>
2014-04-09 11:54:38 -0400

    Get console output from feed validator and examine for errors/warnings.
    Add field with validation summary to GTFSFeed model.
    Check validity (valid if no errors) and write to is_valid field.
    Saves results as text file of console output, instead of HTML.

--- python/django/transit_indicators/tasks.py
+++ python/django/transit_indicators/tasks.py
@@ -1,5 +1,5 @@
 from os import path
-from subprocess import Popen
+import subprocess
 
 from celery.utils.log import get_task_logger
 from django.conf import settings
@@ -21,7 +21,7 @@ def verify_gtfs(feed_id):
     source_file_name = feed.source_file.name
     gtfs_path = path.join(settings.MEDIA_ROOT, path.normpath(source_file_name))
     logger.info('Verifying GTFS %s', gtfs_path)
-    output_file_name = path.splitext(path.basename(source_file_name))[0] + '.html'
+    output_file_name = path.splitext(path.basename(source_file_name))[0] + '.txt'
     logger.info('Output file name: %s', output_file_name)
     output_path = path.join(settings.MEDIA_ROOT, output_file_name)
     logger.info('Writing validation output to %s', output_path)
@@ -31,36 +31,43 @@ def verify_gtfs(feed_id):
         return False
 
     logger.debug('Got input file.  Validating...')
-    p = Popen(['feedvalidator.py', '-m', '-n', '-o', output_path, gtfs_path])
+    p = subprocess.Popen(['feedvalidator.py', '-m', '-n', '-o', 'CONSOLE', gtfs_path],
+                         stdout=subprocess.PIPE)
     stdout, stderr = p.communicate()
+    
     if stdout:
         logger.debug(stdout)
+        result = stdout.split('\n')
+        errct = result[-2:-1][0] # output line with count of errors/warnings
+        if errct.find('error') > -1:
+            logger.info('found error(s): %s', errct)
+            feed.is_valid = False
+        else:
+            feed.is_valid = True
+            if errct.find('successfully') > -1:
+                logger.info('no errors or warnings found.')
+            else:
+                # have warnings
+                logger.info('found warnings: ' + errct[7:])
+                
+        feed.validation_summary = errct
+        
+        with open(output_path, 'wb') as f:
+            f.write(stdout)
+
+        with open(output_path, 'rb') as f:
+            feed.validation_results_file.save(output_path, File(f), save=False)
+            
+        feed.save()
+    else:
+        logger.error('no output returned from validation run!')
+        return False
 
     if stderr:
         logger.error(stderr)
-
-    inspect_validation_output(output_path, feed)
-    logger.debug('Finished in verify_gtfs task.')
-    return True
-
-
-def inspect_validation_output(validation_file, feed):
-    """Helper function to look at output from feedvalidator.py, and update GTFSFeed object.
-
-    Arguments:
-    validation_file:  Full path to html file output from feedvalidator.py
-    feed:  GTFSFeed model instance (will update is_valid and validation file path)
-    """
-    if not path.isfile(validation_file):
-        logger.error('File %s does not exist in inspect_validation_output.', validation_file)
         return False
 
-    with open(validation_file, 'rb') as f:
-        # add Django file object to model instance
-        feed.validation_results_file.save(validation_file, File(f), save=False)
-
-    # TODO: Parse validation output to determine if there were feed errors or not,
-    # then set feed.is_valid accordingly.
-    feed.is_valid = True
-    feed.save()
+    
+    # inspect_validation_output(output_path, feed)
+    logger.debug('Finished in verify_gtfs task.')
     return True
