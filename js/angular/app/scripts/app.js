35a2c4b1dbcb7246ae94986fa4eed9c4d040c7d5
CloudNiner <andrew.fink.11@gmail.com>
2014-05-01 17:15:37 -0400

    Add angular ui router to project
    Move existing gtfs upload view to nested folder
    Install angular-ui-router 0.2.10
    Ran grunt build to update build deps in index.html
        The bootstrap-sass-official includes were missing?

--- js/angular/app/scripts/app.js
+++ js/angular/app/scripts/app.js
@@ -1,9 +1,20 @@
 'use strict';
 
-angular
-  .module('transitIndicators', [
-      'ngCookies',
-      'ngResource',
-      'angularFileUpload',
-      'ui.bootstrap'
-  ]);
+angular.module('transitIndicators', [
+    'ngCookies',
+    'ngResource',
+    'ui.router',
+    'angularFileUpload',
+    'ui.bootstrap'
+]).config(['$stateProvider', '$urlRouterProvider',
+        function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/upload/');
+    $stateProvider
+        .state('upload', {
+            url: '/upload/',
+            templateUrl: 'scripts/modules/upload/gtfs/gtfs-upload-partial.html',
+            controller: 'GTFSUploadController'
+        });
+}]);
+
