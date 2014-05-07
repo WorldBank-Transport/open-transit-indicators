e8473799fbac774b49e19a8b2dfe6b2af4330e6e
CloudNiner <andrew.fink.11@gmail.com>
2014-05-07 13:24:00 -0400

    Documentation for settings view routing

--- js/angular/app/scripts/app.js
+++ js/angular/app/scripts/app.js
@@ -27,8 +27,23 @@ angular.module('transitIndicators', [
             controller: 'OTISettingsController'
         });
 
-        // URL matches to /settings/<view.id>
-        // Add new entries to scripts/config.js in settingsViews
+        /*
+         * config.settingsView view objects are:
+         * {
+         *      id: '<unique string>',
+         *      label: '<sidebar display string>'
+         * }
+         * URL matches to /settings/<view.id>
+         * To add a new entry in the settings sidebar:
+         *  1) Add new 'view' object to config.settingsView in config.js with
+         *     id and label properties
+         *  2) Add controller and partial files to the path:
+         *     modules/settings/<view.id>/<view.id>-controller.js
+         *     modules/settings/<view.id>/<view.id>-partial.html
+         *  3) Ensure controller has the name OTI<view.id>Controller where the first letter of
+         *     view.id is capitalized, e.g. for view.id == upload, name is OTIUploadController
+         *  4) Add <script> tag for your controller in ../index.html
+         */
         _.each(config.settingsViews, function (view) {
             var viewId = view.id;
             var capsId = viewId.charAt(0).toUpperCase() + viewId.slice(1);
