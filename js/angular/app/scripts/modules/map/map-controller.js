9e29eb679763717b57e932360c6683b145c4742b
Kathryn Killebrew <kkillebrew@azavea.com>
2014-07-09 10:24:16 -0400

    Remove comments

--- js/angular/app/scripts/modules/map/map-controller.js
+++ js/angular/app/scripts/modules/map/map-controller.js
@@ -32,56 +32,16 @@ angular.module('transitIndicators')
         }
     };
     
-    /* The directive approach (using layer instead)
-    var utfgrid = {
-          name: 'GTFS Stops Interactivity',
-          type: 'utfGrid',
-          url: windshaftHost + '/tiles/transit_indicators/table/gtfs_stops/{z}/{x}/{y}.grid.json',
-          pluginOptions: { 'useJsonP': false }
-    }; 
-    */
-    
     var leaflet = {
         layers: angular.extend(config.leaflet.layers, layers)
-        //utfgrid: utfgrid
     };
     $scope.leaflet = angular.extend(config.leaflet, leaflet);
     
     $scope.stopLabel = "";
     
     $scope.$on('leafletDirectiveMap.utfgridMouseover', function(event, leafletEvent) {
-        /*
-        console.log(event);
-        console.log(leafletEvent);
-        */
         $scope.stopLabel = leafletEvent.data.stop_desc;
     });
-    
-    /* Logging some things that might be useful to look at in future
-    $scope.myMap = leafletData.getMap().then(function(map) {
-      console.log(map);
-    });
-    
-    $scope.$on('leafletDirectiveMap.mouseover', function(event, leafletEvent) {
-        console.log("moused over map!");
-        console.log(event);
-        console.log(leafletEvent);
-    });
-    
-    $scope.myUTFGrid = leafletData.getUTFGrid().then(function(utf) {
-      console.log('got utf...');
-      console.log(utf);
-      
-      utf.on('mouseover', function(e) {
-          console.log(e);
-      });
-    });
-    
-    $scope.myLayers = leafletData.getLayers().then(function(layers) {
-        console.log(layers);
-    });
-
-    */
     
 }]);
 
