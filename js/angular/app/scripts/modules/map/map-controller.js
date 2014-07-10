c59e9b1e7cc89dc72f9ed91d31ee51ea9277d55a
Kenny Shepard <kshepard@azavea.com>
2014-07-10 16:32:08 -0400

    Zoom to data extent on load and GTFS upload

--- js/angular/app/scripts/modules/map/map-controller.js
+++ js/angular/app/scripts/modules/map/map-controller.js
@@ -2,8 +2,8 @@
 
 angular.module('transitIndicators')
 .controller('OTIMapController',
-        ['config', '$scope', '$state', '$location', 'leafletData',
-        function (config, $scope, $state, $location, leafletData) {
+        ['config', '$scope', '$state', '$location', 'leafletData', 'OTIMapService',
+        function (config, $scope, $state, $location, leafletData, mapService) {
 
     $scope.$state = $state;
 
@@ -33,7 +33,7 @@ angular.module('transitIndicators')
                 visible: true,
                 pluginOptions: { 'useJsonP': false }
             }
-            
+
         }
     };
 
@@ -41,7 +41,7 @@ angular.module('transitIndicators')
         layers: angular.extend(config.leaflet.layers, layers)
     };
     $scope.leaflet = angular.extend(config.leaflet, leaflet);
-    
+
     $scope.$on('leafletDirectiveMap.utfgridClick', function(event, leafletEvent) {
         // we need something to bind the popup to, so use a marker with an empty icon
         if (!$scope.leaflet.markers) {
@@ -53,7 +53,7 @@ angular.module('transitIndicators')
                 $scope.leaflet.markers = null;
                 return;
             }
-            
+
             $scope.$apply(function() {
                 var marker = {
                     lat: leafletEvent.latlng.lat,
@@ -71,12 +71,27 @@ angular.module('transitIndicators')
                 $scope.leaflet.markers = { stopMarker : marker };
             });
         }
-        
+
     });
-    
+
     $scope.$on('leafletDirectiveMap.utfgridMouseout', function(event, leafletEvent) {
         $scope.leaflet.markers = null;
     });
-    
-}]);
 
+    // asks the server for the data extent and zooms to it
+    var zoomToDataExtent = function () {
+        mapService.getMapInfo().then(function(mapInfo) {
+            if (mapInfo.extent) {
+                $scope.leaflet.bounds = mapInfo.extent;
+            }
+        });
+    };
+
+    // always zoom to the extent when the map is first loaded
+    zoomToDataExtent();
+
+    // zoom to the new extent whenever a GTFS file is uploaded
+    $scope.$on('upload-controller:gtfs-uploaded', function() {
+        zoomToDataExtent();
+    });
+}]);
--- js/angular/app/scripts/modules/map/map-service.js
+++ js/angular/app/scripts/modules/map/map-service.js
@@ -2,10 +2,21 @@
 
 angular.module('transitIndicators')
 .factory('OTIMapService',
-        [
-        function () {
+        ['$q', '$resource',
+        function ($q, $resource) {
 
     var otiMapService = {};
+
+    // retrieves map information from the server
+    otiMapService.getMapInfo = function() {
+        var r = $resource('/gt/map-info');
+        var dfd = $q.defer();
+
+        var result = r.get({}, function() {
+            dfd.resolve(result);
+        });
+        return dfd.promise;
+    };
 
     return otiMapService;
 }]);
