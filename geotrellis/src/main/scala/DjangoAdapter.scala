8f114b180a9b3a1c235066cabc747f4300dfcfe9
Kenny Shepard <kshepard@azavea.com>
2014-08-08 12:15:43 -0400

    Add support for POSTing indicators for storage

---
+++ geotrellis/src/main/scala/DjangoAdapter.scala
@@ -0,0 +1,146 @@
+package opentransitgt
+
+import akka.actor.ActorSystem
+import com.github.nscala_time.time.Imports._
+import org.joda.time.format.ISODateTimeFormat
+import scala.concurrent.Future
+import scala.util.Failure
+import scala.util.Success
+
+// JSON support
+import spray.json._
+import spray.json.AdditionalFormats
+import spray.httpx.SprayJsonSupport
+import SprayJsonSupport._
+import DefaultJsonProtocol._
+
+// HTTP client support
+import spray.client._
+import spray.client.pipelining._
+import spray.http.HttpMethods._
+import spray.http.HttpHeaders._
+import spray.http.HttpRequest
+import spray.http.HttpResponse
+import spray.http.MediaTypes._
+
+
+// Fosters communication between GeoTrellis and Django via case classes,
+// formatters, and methods for transferring JSON data.
+object DjangoAdapter {
+  // Endpoint URIs
+  val BASE_URI = "http://localhost/api"
+  val INDICATOR_URI = s"$BASE_URI/indicators/"
+
+  // Sample period parameters
+  case class SamplePeriod(
+    id: Int,
+    period_start: DateTime,
+    period_end: DateTime
+  )
+
+  // Calculation request parameters
+  case class CalcParams(
+    token: String,
+    version: Int,
+    sample_periods: List[SamplePeriod]
+  )
+
+  // Indicator
+  case class Indicator(
+    `type`: String,
+    sample_period: Int,
+    aggregation: String = "system",
+    route_id: String = "",
+    route_type: Int = 0,
+    city_bounded: Boolean = false,
+    version: Int = 0,
+    value: Double = 0
+  )
+
+  // Custom JSON formatters
+  object JsonImplicits extends DefaultJsonProtocol with SprayJsonSupport {
+    // DateTime (nscala-time) isn't parsed by default, so we need to write our own
+    implicit object DateTimeFormat extends RootJsonFormat[DateTime] {
+      private val isoParser = ISODateTimeFormat.dateTimeNoMillis();
+      def write(dt: DateTime) = JsString(isoParser.print(dt))
+      def read(value: JsValue) = value match {
+        case JsString(s) => isoParser.parseDateTime(s)
+        case _ => throw new DeserializationException(s"Error parsing DateTime: $value")
+      }
+    }
+
+    // Use built-in JSON formats for our case classes
+    implicit val samplePeriodFormat = jsonFormat3(SamplePeriod)
+    implicit val calcParamsFormat = jsonFormat3(CalcParams)
+    implicit val indicatorFormat = jsonFormat8(Indicator)
+  }
+
+  // Execution context for futures
+  import system.dispatcher
+
+  // Bring the actor system in scope
+  implicit val system = ActorSystem()
+
+  // Class for interfacing with the Django API
+  class DjangoClient(implicit system: ActorSystem) {
+    // Pipeline for sending HTTP requests and receiving responses
+    val pipeline: HttpRequest => Future[HttpResponse] = sendReceive
+
+    // Sends a POST request to the indicators endpoint
+    def postIndicator(token: String, indicator: Indicator) = {
+      import JsonImplicits._
+
+      val post = Post(INDICATOR_URI, indicator) ~> addHeader("Authorization", s"Token $token")
+      pipeline(post).map(_.entity.asString) onComplete {
+        case Success(response) => println(response)
+        case Failure(error) => println("An error has occured: " + error.getMessage)
+      }
+    }
+  }
+
+  // Create the client used for sending requests to Django
+  val djangoClient = new DjangoClient()(system)
+
+  // Sends all indicator calculations to Django for storage
+  def storeIndicators(token: String, version: Int, period: Int, calc: IndicatorsCalculator) = {
+    // Number of routes
+    for ((mode, value) <- calc.numRoutesPerMode) {
+      djangoClient.postIndicator(token, Indicator(
+        `type`="num_routes", sample_period=period, aggregation="mode",
+        route_type=mode, version=version,value=value)
+      )
+    }
+
+    // Stops per route
+    for ((route, value) <- calc.maxStopsPerRoute) {
+      djangoClient.postIndicator(token, Indicator(
+        `type`="num_stops", sample_period=period, aggregation="route",
+        route_id=route, version=version,value=value)
+      )
+    }
+
+    // Stops per mode
+    for ((mode, value) <- calc.numStopsPerMode) {
+      djangoClient.postIndicator(token, Indicator(
+        `type`="num_stops", sample_period=period, aggregation="mode",
+        route_type=mode, version=version,value=value)
+      )
+    }
+
+    // Length per route
+    for ((route, value) <- calc.maxTransitLengthPerRoute) {
+      djangoClient.postIndicator(token, Indicator(
+        `type`="length", sample_period=period, aggregation="route",
+        route_id=route, version=version,value=value)
+      )
+    }
+
+    // Length per mode
+    for ((mode, value) <- calc.avgTransitLengthPerMode) {
+      djangoClient.postIndicator(token, Indicator(
+        `type`="length", sample_period=period, aggregation="mode",
+        route_type=mode, version=version,value=value)
+      )
+    }
+  }
+}
