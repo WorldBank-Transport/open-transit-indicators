bd114495e5a62d02946f31ddc84c8e1f479a8f10
CloudNiner <andrew.fink.11@gmail.com>
2014-08-18 15:59:42 -0400

    Calculate indicator geometry in scala for route aggregations
    Geom for an indicator should be calculated with the indicators
    themselves, and POSTed to the Indicator API endpoint.
    
    Geom can be an empty string if not relevant.
    
    TODO: Calculate indicator geom for mode aggregations

--- geotrellis/src/main/scala/DjangoAdapter.scala
+++ geotrellis/src/main/scala/DjangoAdapter.scala
@@ -55,7 +55,8 @@ object DjangoAdapter {
     route_type: Int = 0,
     city_bounded: Boolean = false,
     version: Int = 0,
-    value: Double = 0
+    value: Double = 0,
+    the_geom: String = ""
   )
 
   // Custom JSON formatters
@@ -73,7 +74,7 @@ object DjangoAdapter {
     // Use built-in JSON formats for our case classes
     implicit val samplePeriodFormat = jsonFormat4(SamplePeriod)
     implicit val calcParamsFormat = jsonFormat3(CalcParams)
-    implicit val indicatorFormat = jsonFormat8(Indicator)
+    implicit val indicatorFormat = jsonFormat9(Indicator)
   }
 
   // Execution context for futures
@@ -106,11 +107,20 @@ object DjangoAdapter {
   //
   // TODO: refactor this to use some sort of introspection system so it is
   //   no longer necessary to individually store each indicator.
-  def storeIndicators(token: String, version: Int, period: String, calc: IndicatorsCalculator) = {
+  // TODO: Add the_geom for mode indicators
+  def storeIndicators(token: String, version: Int, period: SamplePeriod, calc: IndicatorsCalculator) = {
+
+    def stringGeomForRouteId(routeID: String) = {
+        calc.lineForRouteID(routeID) match {
+            case None => ""
+            case Some(routeLine) => routeLine.toString
+        }
+    }
+
     // Number of routes
     for ((mode, value) <- calc.numRoutesPerMode) {
       djangoClient.postIndicator(token, Indicator(
-        `type`="num_routes", sample_period=period, aggregation="mode",
+        `type`="num_routes", sample_period=period.`type`, aggregation="mode",
         route_type=mode, version=version,value=value)
       )
     }
@@ -118,15 +128,17 @@ object DjangoAdapter {
     // Stops per route
     for ((route, value) <- calc.maxStopsPerRoute) {
       djangoClient.postIndicator(token, Indicator(
-        `type`="num_stops", sample_period=period, aggregation="route",
-        route_id=route, version=version,value=value)
+        `type`="num_stops", sample_period=period.`type`, aggregation="route",
+        route_id=route, version=version,value=value,
+        the_geom=stringGeomForRouteId(route)
+        )
       )
     }
 
     // Stops per mode
     for ((mode, value) <- calc.numStopsPerMode) {
       djangoClient.postIndicator(token, Indicator(
-        `type`="num_stops", sample_period=period, aggregation="mode",
+        `type`="num_stops", sample_period=period.`type`, aggregation="mode",
         route_type=mode, version=version,value=value)
       )
     }
@@ -134,15 +146,16 @@ object DjangoAdapter {
     // Length per route
     for ((route, value) <- calc.maxTransitLengthPerRoute) {
       djangoClient.postIndicator(token, Indicator(
-        `type`="length", sample_period=period, aggregation="route",
-        route_id=route, version=version,value=value)
+        `type`="length", sample_period=period.`type`, aggregation="route",
+        route_id=route, version=version,value=value,
+        the_geom=stringGeomForRouteId(route))
       )
     }
 
     // Length per mode
     for ((mode, value) <- calc.avgTransitLengthPerMode) {
       djangoClient.postIndicator(token, Indicator(
-        `type`="length", sample_period=period, aggregation="mode",
+        `type`="length", sample_period=period.`type`, aggregation="mode",
         route_type=mode, version=version,value=value)
       )
     }
@@ -150,15 +163,16 @@ object DjangoAdapter {
     // Avg time between stops per route
     for ((route, value) <- calc.avgTimeBetweenStopsPerRoute) {
       djangoClient.postIndicator(token, Indicator(
-        `type`="time_traveled_stops", sample_period=period, aggregation="route",
-        route_id=route, version=version, value=value)
+        `type`="time_traveled_stops", sample_period=period.`type`, aggregation="route",
+        route_id=route, version=version, value=value,
+        the_geom=stringGeomForRouteId(route))
       )
     }
 
     // Avg time between stops per mode
     for ((mode, value) <- calc.avgTimeBetweenStopsPerMode) {
       djangoClient.postIndicator(token, Indicator(
-        `type`="time_traveled_stops", sample_period=period, aggregation="mode",
+        `type`="time_traveled_stops", sample_period=period.`type`, aggregation="mode",
         route_type=mode, version=version, value=value)
       )
     }
