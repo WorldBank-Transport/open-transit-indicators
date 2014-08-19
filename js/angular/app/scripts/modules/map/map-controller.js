429c74cee2e824cf552ebd9de3e6b04effc13106
CloudNiner <andrew.fink.11@gmail.com>
2014-08-19 15:02:04 -0400

    Add support for dynamic indicator selection in UI

--- js/angular/app/scripts/modules/map/map-controller.js
+++ js/angular/app/scripts/modules/map/map-controller.js
@@ -7,31 +7,35 @@ angular.module('transitIndicators')
 
     $scope.$state = $state;
 
-    $scope.logout = function () {
-        authService.logout();
+    // Object used to configure the indicator displayed on the map
+    // Will never actually be posted or saved
+    // TODO: Save this indcator somewhere else to save map state on refresh
+    //       or set via page GET params
+    var indicator = {
+        version: 0,
+        type: 'num_stops',
+        sample_period: 'morning',
+        aggregation: 'route'
     };
-
-    var windshaftHost = $location.protocol() + '://' + $location.host();
-    if (config.windshaft && config.windshaft.port) {
-        windshaftHost += ':' + config.windshaft.port;
-    }
 
     /* LEAFLET CONFIG */
     var layers = {
         overlays: {
-            // TODO: Dynamically create/destroy these layers based on UI selection
-            indicators: {
+            indicator: {
                 name: 'GTFS Indicator',
                 type: 'xyz',
-                url: windshaftHost + '/tiles/transit_indicators/1408110474/num_stops/morning/route/{z}/{x}/{y}.png',
-                visible: true
+                url: mapService.getIndicatorUrl('png'),
+                visible: true,
+                layerOptions: indicator
             },
-            indicators_utfgrid: {
+            utfgrid: {
                 name: 'GTFS Indicator Interactivity',
                 type: 'utfGrid',
-                url: windshaftHost + '/tiles/transit_indicators/1408110474/num_stops/morning/route/{z}/{x}/{y}.grid.json?interactivity=value',
+                url: mapService.getIndicatorUrl('utfgrid'),
                 visible: true,
-                pluginOptions: { 'useJsonP': false }
+                // When copied to the internal L.Utfgrid class, these options end up on
+                //  layer.options, same as for TileLayers
+                pluginOptions: angular.extend({ 'useJsonP': false }, indicator)
             }
         }
     };
@@ -41,32 +45,6 @@ angular.module('transitIndicators')
         markers: []
     };
     $scope.leaflet = angular.extend(config.leaflet, leaflet);
-
-    $scope.$on('leafletDirectiveMap.utfgridClick', function(event, leafletEvent) {
-        // we need something to bind the popup to, so use a marker with an empty icon
-
-        $scope.leaflet.markers.length = 0;
-
-        if (leafletEvent.data) {
-            // use $apply so popup appears right away
-            // (otherwise it doesn't show up until the next time the mouse gets moved)
-            $scope.$apply(function() {
-                var marker = {
-                    lat: leafletEvent.latlng.lat,
-                    lng: leafletEvent.latlng.lng,
-                    focus: true,
-                    draggable: false,
-                    message: 'Indicator Value: ' + leafletEvent.data.value,
-                    icon: {
-                        type: 'div',
-                        iconSize: [0, 0],
-                        popupAnchor:  [0, 0]
-                    }
-                };
-                $scope.leaflet.markers.push(marker);
-            });
-        }
-    });
 
     // asks the server for the data extent and zooms to it
     var zoomToDataExtent = function () {
@@ -80,8 +58,72 @@ angular.module('transitIndicators')
         });
     };
 
-    // always zoom to the extent when the map is first loaded
-    zoomToDataExtent();
+    // Create utfgrid popup from leaflet event
+    var utfGridMarker = function (leafletEvent) {
+        if (leafletEvent && leafletEvent.data) {
+            // use $apply so popup appears right away
+            // (otherwise it doesn't show up until the next time the mouse gets moved)
+            $scope.$apply(function() {
+                var marker = {
+                    lat: leafletEvent.latlng.lat,
+                    lng: leafletEvent.latlng.lng,
+                    focus: true,
+                    draggable: false,
+                    message: 'Indicator Value: ' + leafletEvent.data.value,
+                    // we need something to bind the popup to, so use a marker with an empty icon
+                    icon: {
+                        type: 'div',
+                        iconSize: [0, 0],
+                        popupAnchor:  [0, 0]
+                    }
+                };
+                $scope.leaflet.markers.push(marker);
+            });
+        }
+    };
+
+    $scope.setIndicatorVersion = function (version) {
+        $scope.indicator_version = version;
+        $scope.$broadcast('map-controller:set-indicator-version', version);
+    };
+
+    /**
+     * Indicator type to configure the layers with
+     * Use the map object directly to iterate over layers
+     * angular-leaflet-directive does not support a way to redraw existing layers that have
+     * updated properties but haven't changed their layer key
+     *
+     * @param indicator: mapService.Indicator instance
+     */
+    $scope.updateIndicatorLayers = function (indicator) {
+        leafletData.getMap().then(function(map) {
+            map.eachLayer(function (layer) {
+                if (!layer) {
+                    return;
+                }
+                // Its one of the indicator overlays
+                if (layer.options.type) {
+                    angular.extend(layer.options, indicator);
+                    if (layer.redraw) {
+                        layer.redraw();
+                    } else if (layer._update) {
+                        // Temporary hack for Leaflet.Utfgrid. It doesn't have a redraw function
+                        layer._cache = {};
+                        layer._update();
+                    }
+                }
+            });
+        });
+    };
+
+    $scope.logout = function () {
+        authService.logout();
+    };
+
+    $scope.$on('leafletDirectiveMap.utfgridClick', function(event, leafletEvent) {
+        $scope.leaflet.markers.length = 0;
+        utfGridMarker(leafletEvent);
+    });
 
     // zoom to the new extent whenever a GTFS file is uploaded
     $scope.$on('upload-controller:gtfs-uploaded', function() {
@@ -92,4 +134,20 @@ angular.module('transitIndicators')
     $scope.$on('upload-controller:gtfs-deleted', function() {
         $scope.leaflet.bounds = config.worldExtent;
     });
+
+    $scope.$on('map-controller:set-indicator-version', function (event, version) {
+        indicator.version = version;
+        $scope.updateIndicatorLayers(indicator);
+    });
+
+    $scope.init = function () {
+        // always zoom to the extent when the map is first loaded
+        zoomToDataExtent();
+
+        mapService.Indicator.query({}, function(data){
+            var currentIndicator = _.sortBy(data, 'version').pop();
+            $scope.setIndicatorVersion(currentIndicator.version);
+        });
+    };
+    $scope.init();
 }]);
