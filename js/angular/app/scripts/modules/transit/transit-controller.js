829e311a6bdda6cf8367e3e54a80d7ab77db70cc
CloudNiner <andrew.fink.11@gmail.com>
2014-09-05 10:10:19 -0400

    Remove demo legend
    Angular-leaflet-directive only support static legends,
    and cannot add/remove them dynamically.

--- js/angular/app/scripts/modules/transit/transit-controller.js
+++ js/angular/app/scripts/modules/transit/transit-controller.js
@@ -26,10 +26,6 @@ angular.module('transitIndicators')
         }
     };
 
-    $scope.leaflet.legend = {
-        colors: [ '#ff0000', '#28c9ff', '#0000ff', '#ecf386' ],
-        labels: [ 'National Cycle Route', 'Regional Cycle Route', 'Local Cycle Network', 'Cycleway' ]
-    };
     $scope.updateLeafletOverlays(overlays);
 
     $scope.$on('leafletDirectiveMap.utfgridClick', function (event, leafletEvent) {
