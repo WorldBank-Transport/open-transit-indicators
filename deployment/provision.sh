f2d884293489aecece24975229e20b915e4fee96
Chris Brown <cbrown@azavea.com>
2014-09-26 09:09:41 -0400

    Add setting of encodings for proper database instantiation

--- deployment/provision.sh
+++ deployment/provision.sh
@@ -125,6 +125,13 @@ case "$INSTALL_TYPE" in
         ;;
 esac
 
+#########################
+## Ensure locales set correctly
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
 
 #########################
 # Project Dependencies  #
