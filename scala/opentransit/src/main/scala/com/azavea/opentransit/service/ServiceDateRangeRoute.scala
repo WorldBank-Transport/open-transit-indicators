482a0da062769df12f50c4a1302ae8dceda2fec2
Kathryn Killebrew <kkillebrew@azavea.com>
2014-10-13 11:09:17 -0400

    Add error handling to service-dates endpoint

--- scala/opentransit/src/main/scala/com/azavea/opentransit/service/ServiceDateRangeRoute.scala
+++ scala/opentransit/src/main/scala/com/azavea/opentransit/service/ServiceDateRangeRoute.scala
@@ -26,20 +26,32 @@ trait ServiceDateRangeRoute extends Route { self: DatabaseInstance =>
         complete {
           future {
             db withSession { implicit session: Session =>
-              val q = Q.queryNA[ServiceDateRange]("""
-                SELECT MIN(start_date) AS start, MAX(end_date) AS end FROM gtfs_calendar;""")
-              val serviceRange = q.list.head
+              try {
+                val q = Q.queryNA[ServiceDateRange]("""
+                  SELECT MIN(start_date) AS start, MAX(end_date) AS end FROM gtfs_calendar;""")
+                val serviceRange = q.list.head
 
-              // construct the json response, using null if no data is available
-              val serviceRangeJson = 
-                if (serviceRange.start == null || serviceRange.end == null) JsNull else
-                JsObject(
-                  // return dates as ANSI-formatted strings (YYYY-MM-DD)
-                  "start" -> JsString(serviceRange.start),
-                  "end" -> JsString(serviceRange.end)
-                )
-              // return the service date range json
-              JsObject("service-dates" -> serviceRangeJson)
+                // construct the json response, using null if no data is available
+                val serviceRangeJson = 
+                  if (serviceRange.start == null || serviceRange.end == null) JsNull else
+                  JsObject(
+                    // return dates as ANSI-formatted strings (YYYY-MM-DD)
+                    "start" -> JsString(serviceRange.start),
+                    "end" -> JsString(serviceRange.end)
+                  )
+                // return the service date range json
+                JsObject("service-dates" -> serviceRangeJson) 
+              } catch {
+                case e: Exception =>
+                        println("Error checking feed service date range!")
+                        println(e.getMessage)
+                        println(e.getStackTrace.mkString("\n"))
+                        // return error instead
+                        JsObject(
+                          "error" -> JsString("Error getting feed service date range.\n" + 
+                                              e.getMessage.replace("\"", "'"))
+                        )
+              }
             }
           }
         }
