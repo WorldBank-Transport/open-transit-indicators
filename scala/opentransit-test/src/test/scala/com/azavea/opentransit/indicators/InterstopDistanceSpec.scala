b8b36e8e6748db62ddb7d0318613abc6dd0d2911
Sharp Hall <shall@azavea.com>
2014-10-13 11:42:23 -0400

    Fix InterstopDistance indicator and add test for ad-hoc system

---
+++ scala/opentransit-test/src/test/scala/com/azavea/opentransit/indicators/InterstopDistanceSpec.scala
@@ -0,0 +1,23 @@
+package com.azavea.opentransit.indicators
+
+import com.azavea.gtfs._
+
+import com.azavea.opentransit.testkit._
+
+import org.scalatest._
+
+
+class AdHocInterstopDistanceSpec extends AdHocSystemIndicatorSpec {
+  it should "Calculate the average distance between stops in the ad-hoc system" in {
+    val calculation = InterstopDistance.calculation(allStopsPeriod)
+    val AggregatedResults(byRoute, byRouteType, bySystem) = calculation(systemWithAllStops)
+    implicit val routeMap = byRoute // Use this implicit to DRY out your tests
+
+    routeById("EastRail") should be (0.0)
+    routeById("EastBus") should be (0.0)
+    routeById("NorthSouth") should be (0.01 +- 1e-2)
+    routeById("WestRail") should be (0.0)
+    routeById("EastWest") should be (0.01 +- 1e-2)
+    routeById("WestBus") should be (0.0)
+  }
+}
