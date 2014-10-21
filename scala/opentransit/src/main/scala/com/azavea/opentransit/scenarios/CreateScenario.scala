db3a8873b0be49f39fbfaa9370806f3d66bdd687
Eugene Cheipesh <echeipesh@gmail.com>
2014-10-21 17:27:00 -0400

    Changed error handling in CreateScenario

--- scala/opentransit/src/main/scala/com/azavea/opentransit/scenarios/CreateScenario.scala
+++ scala/opentransit/src/main/scala/com/azavea/opentransit/scenarios/CreateScenario.scala
@@ -1,5 +1,7 @@
 package com.azavea.opentransit.scenarios
 
+import grizzled.slf4j.Logging
+
 import scala.slick.jdbc.JdbcBackend.{Database, Session, DatabaseDef}
 import scala.sys.process._
 import com.azavea.gtfs._
@@ -9,31 +11,28 @@ import com.azavea.opentransit._
 import com.azavea.opentransit.io._
 import com.typesafe.config.{ConfigFactory,Config}
 
-object CreateScenario {
-  /** Creates a new scenario and passes status to a sink function.
-    */
-  def apply(request: ScenarioCreationRequest, dbByName: String => Database)
-    (sink: JobStatus => Unit): Unit = {
+import scala.util.Try
+
+object CreateScenario extends Logging {
+  /** Creates a new scenario and passes status to a sink function. */
+  def apply(request: ScenarioCreationRequest, dbByName: String => Database): Unit =  {
+    logger.info(s"Creating scenario with database: ${request.dbName}")
 
     // Initialize the new scenario database via the setup_db script
     val config = ConfigFactory.load
     val dbUser = config.getString("database.user")
     val dbPassword = config.getString("database.password")
 
-    println("Creating new scenario database")
     s"sudo -u postgres ../deployment/setup_db.sh ${request.dbName} $dbUser $dbPassword ..".!!
 
     // Copy GTFS data from the old database to the new database.
-    println(s"Obtaining records from base database: ${request.baseDbName}")
+    logger.info(s"Obtaining gtfs records from base database: ${request.baseDbName}")
     val gtfsRecords = dbByName(request.baseDbName) withSession { implicit session =>
       GtfsRecords.fromDatabase
     }
     // TODO: as an optimization, the sample period may be used here to filter out irrelevant data
-
-    println(s"Pushing to new database: ${request.dbName}")
+    logger.info(s"Pushing gtfs records to new database: ${request.dbName}")
     dbByName(request.dbName) withSession { implicit session => GtfsIngest(gtfsRecords) }
-
-    println("Scenario creation complete, updating status")
-    sink(JobStatus.Complete)
+    logger.info(s"Scenario creation complete: '${request.dbName}'")
   }
 }
--- scala/opentransit/src/main/scala/com/azavea/opentransit/service/ScenariosRoute.scala
+++ scala/opentransit/src/main/scala/com/azavea/opentransit/service/ScenariosRoute.scala
@@ -5,9 +5,10 @@ import com.azavea.opentransit.JobStatus
 import com.azavea.opentransit.JobStatus._
 import com.azavea.opentransit.json._
 import com.azavea.opentransit.scenarios._
+import grizzled.slf4j.Logging
 
 import spray.http.MediaTypes
-import spray.http.StatusCodes.{Created, InternalServerError}
+import spray.http.StatusCodes
 import spray.routing.{ExceptionHandler, HttpService}
 import spray.util.LoggingContext
 import scala.concurrent._
@@ -17,54 +18,30 @@ import spray.httpx.SprayJsonSupport
 import SprayJsonSupport._
 import DefaultJsonProtocol._
 
+import scala.util.{Failure, Success}
+
 case class Scenario(
   dbName: String = "",
   jobStatus: JobStatus
 )
 
-trait ScenariosRoute extends Route { self: DatabaseInstance =>
+trait ScenariosRoute extends Route with Logging { self: DatabaseInstance =>
   // Endpoint for creating a new scenario
   def scenariosRoute = {
     path("scenarios") {
       post {
         entity(as[ScenarioCreationRequest]) { request =>
           complete {
-            var err: JsObject = null
             TaskQueue.execute {
-              try {
-                println(s"Creating scenario with dbName: ${request.dbName}")
-                CreateScenario(request, dbByName) { status =>
-                  DjangoClient.updateScenario(request.token, Scenario(request.dbName, status))
-                }
-                println("Finished creating scenario")
-              } catch {
-              case e: Exception =>
-                println("Error creating scenario!")
-                println(e.getMessage)
-                println(e.getStackTrace.mkString("\n"))
-                err = JsObject(
-                  "success" -> JsBoolean(false),
-                  "message" -> JsString(s"Couldn't create scenario: ${e.getMessage}")
-                )
-
-                try {
-                  DjangoClient.updateScenario(request.token,
-                    Scenario(request.dbName, JobStatus.Failed))
-                } catch {
-                  case ex: Exception =>
-                    println("Failed to update scenario")
-                }
-              }
+              CreateScenario(request, dbByName)
+            }.onComplete{
+              case Success(_) =>
+                DjangoClient.updateScenario(request.token, Scenario(request.dbName, JobStatus.Complete))
+              case Failure(ex) =>
+                DjangoClient.updateScenario(request.token, Scenario(request.dbName, JobStatus.Failed))
             }
 
-            if (err == null) {
-              Created -> JsObject(
-                "success" -> JsBoolean(true),
-                "message" -> JsString(s"Scenario creation started")
-              )
-            } else {
-              err
-            }
+            StatusCodes.Accepted -> successMessage("Scenario creation started.")
           }
         }
       }
