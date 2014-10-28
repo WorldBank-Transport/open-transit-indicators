556ebc81b42c49c3c5272cc8abf8758d2f1b096c
Eugene Cheipesh <echeipesh@gmail.com>
2014-10-28 13:42:30 -0400

    add: TestDatabaseFixture that implements DatabaseInterface for tests

---
+++ scala/opentransit-test/src/test/scala/com/azavea/opentransit/TestDatabaseFixture.scala
@@ -0,0 +1,73 @@
+package com.azavea.opentransit
+
+import com.azavea.gtfs.GtfsRecords
+import com.azavea.opentransit.io.GtfsIngest
+import com.azavea.opentransit.testkit.{TestGtfsRecords, TestFiles, TestDatabase, DatabaseTestFixture}
+
+
+import com.typesafe.config.{ConfigFactory,Config}
+import org.scalatest._
+import spray.http.CacheDirectives.public
+import scala.slick.jdbc.{GetResult, StaticQuery => Q}
+import scala.slick.jdbc.JdbcBackend.{Database, Session}
+import scala.sys.process._
+
+/**
+ * This fixture will setup a default database with name of the test class and pre-populate it.
+ * Any database asked by name will be created with the test class name as prefix.
+ * All databases created during the run-time of this test will be dropped at the end.
+ */
+trait TestDatabaseFixture extends DatabaseInstance with BeforeAndAfterAll  { self: Suite =>
+  var live: Set[String] = Set.empty // set of databases that we have created for this test
+
+  def mainDbName: String = getClass.getSimpleName.toLowerCase
+
+  lazy val (dbSudo, dbUser, dbPassword) = {
+    val config = ConfigFactory.load
+
+    val dbSudo = config.getString("opentransit.testkit.sudo")
+    val dbUser = config.getString("opentransit.testkit.dbuser")
+    val dbPassword = config.getString("opentransit.testkit.dbpassword")
+
+    (dbSudo, dbUser, dbPassword)
+  }
+
+  val db = dbByName(mainDbName)
+
+
+  def dbByName(name: String): Database = {
+    val exempt = List("postgres", mainDbName)
+    val dbName = if (exempt contains name) name else s"$mainDbName-$name"
+
+    Database.forURL(s"jdbc:postgresql:$dbName", driver = "org.postgresql.Driver",
+      user = dbUser, password = dbPassword)
+  }
+
+
+  def createDatabase(name: String) = {
+    val dbName = s"$mainDbName-$name"
+    val logger = ProcessLogger(println, println);
+    s"""sudo -u $dbSudo ../../deployment/setup_db.sh $name "$dbUser" "$dbPassword" ../..""".!!(logger)
+    live += dbName
+  }
+
+  def deleteDatabase(name: String) = {
+    dbByName("postgres") withSession { implicit session: Session =>
+      Q.updateNA( s"""DROP DATABASE IF EXISTS "$name";""").execute
+    }
+  }
+
+  abstract override protected def beforeAll(): Unit = {
+    createDatabase(mainDbName)
+    live += mainDbName
+    dbByName(mainDbName) withSession { implicit session: Session =>
+      GtfsIngest(TestGtfsRecords())
+    }
+  }
+
+  abstract override protected def afterAll(): Unit = {
+    live.foreach(deleteDatabase)
+    super.afterAll()
+  }
+}
+
