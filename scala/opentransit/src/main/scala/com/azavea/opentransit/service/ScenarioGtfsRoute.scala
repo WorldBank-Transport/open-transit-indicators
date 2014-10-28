aea9e47743790d801aa50a074326c98645b3f11d
Eugene Cheipesh <echeipesh@gmail.com>
2014-10-28 13:46:22 -0400

    move JsonFormat object out of the routes file, to resolve implicit resolution problem

--- scala/opentransit/src/main/scala/com/azavea/opentransit/service/ScenarioGtfsRoute.scala
+++ scala/opentransit/src/main/scala/com/azavea/opentransit/service/ScenarioGtfsRoute.scala
@@ -2,6 +2,7 @@ package com.azavea.opentransit.service
 
 import com.azavea.gtfs.io.database.{DatabaseRecordImport, DefaultProfile, GtfsTables}
 import com.azavea.gtfs._
+import com.azavea.opentransit.service.json.ScenariosGtfsRouteJsonProtocol
 import com.azavea.opentransit.{TaskQueue, DatabaseInstance}
 import com.sun.xml.internal.ws.encoding.soap.DeserializationException
 import geotrellis.slick.Projected
@@ -10,9 +11,10 @@ import geotrellis.vector.json.GeoJsonSupport
 import org.joda.time._
 import org.joda.time.format.PeriodFormatterBuilder
 import spray.http._
+import spray.httpx.SprayJsonSupport
+
 import spray.json.DefaultJsonProtocol
 import spray.json._
-import spray.httpx.SprayJsonSupport
 import scala.concurrent._
 import scala.concurrent.ExecutionContext.global
 import scala.util.{Success, Try, Failure}
@@ -23,14 +25,14 @@ case class TripTuple(trip: TripRecord,
                      shape: Option[TripShape])
 
 
-trait ScenarioGtfsRoute extends Route with SprayJsonSupport {
+trait ScenarioGtfsRoute extends Route {
   import ScenarioGtfsRoute._
+  import spray.httpx.SprayJsonSupport._
+  import ScenariosGtfsRouteJsonProtocol._
+
   import tables.profile.simple._
 
   /** This seems weird, but scalac will NOT find this implicit with simple import */
-  implicit val tripPatternFormat = ScenariosGtfsRouteJsonProtocol.tripTupleFormat
-  implicit val routeFormat = ScenariosGtfsRouteJsonProtocol.routeFormat
-  import DefaultJsonProtocol._ // this handles arrays and futures
 
   def scenarioGtfsRoute(scenarioDB: Database) =
     pathPrefix("routes") {
@@ -142,7 +144,7 @@ object ScenarioGtfsRoute {
   import tables.profile.simple._
 
   /** Load all stop_times for route and use them to group trip_ids by trip path */
-  private def fetchTripBins(routeId: String)(implicit s: Session): Array[Array[TripId]] = {
+  private def fetchTripBins(routeId: String)(implicit s: Session): Array[Array[String]] = {
     /** extract part of trip that identifies unique path */
     def tripOffset(stopTimes: Seq[StopTimeRecord]) = {
       import com.github.nscala_time.time.Imports._
@@ -218,136 +220,4 @@ object ScenarioGtfsRoute {
   }
 }
 
-object ScenariosGtfsRouteJsonProtocol extends GeoJsonSupport with DefaultJsonProtocol {
 
-  /** We use this prefix when to generate stop names when saving from a POST request */
-  final val STOP_PREFIX = "TEMP"
-
-  /** This REST API has no concept of service, so we synthesize */
-  final val TRIP_SERVICE_ID = "ALWAYS"
-
-  implicit object routeTypeFormat extends JsonFormat[RouteType]{
-    def read(json: JsValue): RouteType = json match {
-      case JsString(name) => RouteType(name)
-      case _ => throw new DeserializationException("RouteType index expected")
-    }
-
-    def write(obj: RouteType): JsValue =
-      JsString(obj.name)
-  }
-
-  implicit val routeFormat = jsonFormat9(RouteRecord)
-  
-  implicit object periodFormat extends JsonFormat[Period] {
-    val formatter = new PeriodFormatterBuilder()
-      .minimumPrintedDigits(2)
-      .printZeroAlways()
-      .appendHours().appendSuffix(":")
-      .appendMinutes().appendSuffix(":")
-      .appendSeconds()
-      .toFormatter
-
-    def read(json: JsValue): Period = json match {
-      case JsString(period) => formatter.parsePeriod(period)
-      case _ => throw new DeserializationException("Period in hh:mm:ss expected")
-    }
-
-    def write(obj: Period): JsValue = JsString(obj.toString(formatter))
-  }
-
-  implicit object durationFormat extends JsonFormat[Duration] {
-    def read(json: JsValue): Duration = json match {
-      case JsNumber(seconds) => Duration.standardSeconds(seconds.toInt)
-      case _ => throw new DeserializationException("Duration in seconds expected")
-    }
-    def write(obj: Duration): JsValue = JsNumber(obj.toStandardSeconds.getSeconds)
-  }
-
-  implicit object frequencyFormat extends JsonWriter[FrequencyRecord] {
-    def read(json: JsValue)(tripId: String): FrequencyRecord =
-      json.asJsObject.getFields("start", "end", "headway") match {
-        case Seq(start, end, headway) =>
-          FrequencyRecord(tripId, start.convertTo[Period], end.convertTo[Period], headway.convertTo[Duration])
-        case _ => throw new DeserializationException("Frequency expected")
-      }
-
-    def write(obj: FrequencyRecord) = JsObject(
-      "start" -> obj.start.toJson,
-      "end" -> obj.start.toJson,
-      "headway" -> obj.start.toJson
-    )
-  }
-
-  implicit object stopFormat extends JsonWriter[Stop]{
-    def read(json: JsValue)(tripId: String, seq: Int): Stop =
-      json.asJsObject.getFields("stop_id","name","lat","long") match {
-        case Seq(JsString(stopId), JsString(name), JsNumber(lat), JsNumber(long)) =>
-          Stop(s"${STOP_PREFIX}-${tripId}-${seq}", name, None, Projected(Point(long.toDouble, lat.toDouble), 4326))
-        case _ => throw new DeserializationException("Stop expected")
-      }
-
-    def write(obj: Stop): JsValue = JsObject(
-      "stop_id" -> obj.id.toJson,
-      "name" -> obj.name.toJson,
-      "lat" -> obj.point.geom.y.toJson,
-      "long" -> obj.point.geom.x.toJson
-    )
-  }
-
-  implicit object stopTimeFormat extends JsonWriter[(StopTimeRecord, Stop)]{
-    def read(json: JsValue)(tripId: String): (StopTimeRecord, Stop) =
-    json.asJsObject.getFields("stop", "stop_sequence", "arrival_time", "departure_time") match {
-      case Seq(stopJson: JsObject, JsNumber(seq), arrival, departure) =>
-        val stop = stopFormat.read(stopJson)(tripId, seq.toInt)
-        val st = StopTimeRecord(stop.id, tripId, seq.toInt, arrival.convertTo[Period], departure.convertTo[Period])
-        st -> stop
-      case _ =>  throw new DeserializationException("Stop Time expected")
-    }
-
-    def write(obj: (StopTimeRecord, Stop)): JsValue = {
-      val (st, stop) = obj
-      JsObject(
-        "stop_sequence" -> st.sequence.toJson,
-        "arrival_time" -> st.arrivalTime.toJson,
-        "departure_time" -> st.departureTime.toJson,
-        "stop" -> stop.toJson
-      )
-    }
-  }
-
-  implicit object shapeFormat extends JsonWriter[Option[TripShape]]{
-    def write(obj: Option[TripShape]): JsValue =
-      obj match {
-        case Some(shape) => shape.line.geom.toJson
-        case None => JsNull
-      }
-
-    def read(json: JsValue)(tripId: String): Option[TripShape] = json match {
-      case _: JsObject => Some(TripShape(s"${STOP_PREFIX}-${tripId}", Projected(json.convertTo[Line], 4326)))
-      case JsNull => None
-      case _ => throw new DeserializationException("Shape line expected")
-    }
-  }
-
-  val tripTupleFormat = new RootJsonFormat[TripTuple] {
-    def read(json: JsValue): TripTuple =
-      json.asJsObject.getFields("trip_id", "route_id", "headsign", "stop_times", "frequencies", "shape") match {
-        case Seq(JsString(tripId), JsString(routeId), headsign, JsArray(stopTimesJson) , JsArray(freqsJson), shapeJson) =>
-          val stopTimes = stopTimesJson map { js => stopTimeFormat.read(js)(tripId) }
-          val freqs = freqsJson map { js => frequencyFormat.read(js)(tripId) }
-          val shape = shapeFormat.read(shapeJson)(tripId)
-          val trip = TripRecord(tripId, TRIP_SERVICE_ID, routeId, headsign.convertTo[Option[String]], shape.map(_.id))
-          TripTuple(trip, stopTimes, freqs, shape)
-        case _ =>  throw new DeserializationException("TripTuple expected")
-      }
-
-    def write(obj: TripTuple): JsValue = JsObject(
-      "trip_id" -> JsString(obj.trip.id),
-      "route_id" -> JsString(obj.trip.routeId),
-      "headsign" -> JsString(obj.trip.headsign.getOrElse("")),
-      "stop_times" -> JsArray(obj.stopTimes map (_.toJson): _*),
-      "frequencies" -> JsArray( obj.frequencies map (_.toJson):_*),
-      "shape" -> obj.shape.toJson
-    )
-  }
-}
