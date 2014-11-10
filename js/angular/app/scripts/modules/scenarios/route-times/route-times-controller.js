dffa608fa6464478289a8ae3bcc247b42d830919
CloudNiner <andrew.fink.11@gmail.com>
2014-11-10 17:24:10 -0500

    Set and validate scenario stop times
    TODO: Autocreate stop names if they don't exist?
    TODO: Fix tbody width

--- js/angular/app/scripts/modules/scenarios/route-times/route-times-controller.js
+++ js/angular/app/scripts/modules/scenarios/route-times/route-times-controller.js
@@ -4,53 +4,62 @@
 
 angular.module('transitIndicators')
 .controller('OTIScenariosRoutetimesController',
-            ['config', '$scope', '$state', '$stateParams', 'OTIScenariosService', 'leafletData', 'OTIDrawService',
-            function (config, $scope, $state, $stateParams, OTIScenariosService, leafletData, OTIDrawService) {
+            ['config', '$scope', '$state', '$stateParams', 'leafletData',
+             'OTIDrawService', 'OTIScenarioManager', 'OTITripManager',
+            function (config, $scope, $state, $stateParams, leafletData,
+                      OTIDrawService, OTIScenarioManager, OTITripManager) {
 
     // LOCAL
 
+    var DEFAULT_HEADWAY_MINS = 10;
+
     var pad = function (number) {
-      if ( number < 10 ) {
-        return '0' + number;
-      }
-      return number;
+        if ( number < 10 ) {
+            return '0' + number;
+        }
+        return number;
+    };
+
+    var intialize = function () {
+
+        var scenario = OTIScenarioManager.get();
+        $scope.trip = OTITripManager.get();
+
+        leafletData.getMap().then(function (map) {
+            map.addLayer(OTIDrawService.drawnItems);
+        });
+
+        var samplePeriod = _.find($scope.samplePeriods, function (p) {
+            return p.type === scenario.sample_period;
+        });
+        var periodStart = new Date(samplePeriod.period_start);
+        _.each($scope.trip.stopTimes, function (stopTime, index) {
+            if (!stopTime.arrivalTime) {
+                var stopArrivalTime = new Date(periodStart.getTime() + 1000 * 60 * DEFAULT_HEADWAY_MINS * index);
+                var stopArrivalString = pad(stopArrivalTime.getHours()) + ':' + pad(stopArrivalTime.getMinutes()) + ':00';
+                stopTime.arrivalTime = stopArrivalString;
+                stopTime.departureTime = stopArrivalString;
+            }
+        });
+    };
+
+    $scope.updateHeight = function () {
+        $scope.$emit('updateHeight');
     };
 
     // $SCOPE
-
-    $scope.scenario = OTIScenariosService.otiScenario;
-    $scope.route = OTIScenariosService.otiRoute;
-
     $scope.continue = function () {
-        $state.go('route-done');
-        leafletData.getMap().then(function (map) {
-            map.removeLayer(OTIDrawService.drawnItems);
-        });
+        if ($scope.tripStopTimesForm.$valid) {
+            $state.go('route-done');
+            leafletData.getMap().then(function (map) {
+                map.removeLayer(OTIDrawService.drawnItems);
+            });
+        }
     };
 
     $scope.back = function () {
         $state.go('route-shapes');
     };
 
-
-    // INIT
-
-    leafletData.getMap().then(function (map) {
-        map.addLayer(OTIDrawService.drawnItems);
-    });
-
-    // Set sample stop times if not already defined in route
-    var samplePeriod = _.find($scope.samplePeriods, function (p) {
-        return p.type === $scope.scenario.samplePeriod;
-    });
-    _.each($scope.route.stops, function (stop, index) {
-        if (!stop.stopTime.arrivalTime) {
-            var periodStart = new Date(samplePeriod.period_start);
-            var stopArrivalTime = new Date(periodStart.getTime() + 1000 * 60 * $scope.route.headway * index);
-            var stopArrivalString = pad(stopArrivalTime.getHours()) + ':' + pad(stopArrivalTime.getMinutes());
-            stop.stopTime.arrivalTime = stopArrivalString;
-            stop.stopTime.departureTime = stopArrivalString;
-        }
-    });
-
+    intialize();
 }]);
